06281bb84aad277a9fec4edd7aa5f75a7b57e06b
4jamesccraven <4jamesccraven@gmail.com>
2025-04-19 06:20:18 -0400

    added a flake, I intend to clean this repo up soon

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            python313Packages.pygame
+          ];
+        };
+      }
+    );
+}
